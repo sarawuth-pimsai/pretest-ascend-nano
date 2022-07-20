af90fe21b59100c3b2b85e0de162e701a3b992ba
Sarawuth Pimsai <sarawuth.pimsai@gmail.com>
2022-07-20 14:49:52 +0700

    Update flow

--- pairNumbers.js
+++ pairNumbers.js
@@ -8,7 +8,38 @@ function pair(input, arr, begin, end, result) {
   pair(input, arr, begin, --end, result);
   return result;
 }
+
 const arr = [1, 2, 3, 4, 5];
+console.time("a")
 const result = pair(7, arr, 0, arr.length - 1, []);
+console.timeEnd("a")
 const output = result.map((r) => `${r[0]}+${r[1]}`).join(", ");
 console.log(output);
+
+const transData = (array) => {
+  let result = {}
+  for(let i = 0; i<array.length; i++) {
+    const key = array[i]
+    if(!result[key]) {
+      result[key] = array[i]
+    } else {
+      result[key].push(array[i])
+    }
+  }
+  return result
+}
+
+const pair2 = (array, sum) => {
+  let result = []
+  const data = transData(array)
+  for(let i = 0; i < array.length; i++) {
+    const key = sum - array[i]
+    if(data[key]) result.push([array[i], key]) 
+  }
+  return result
+}
+console.time("b")
+const result2 = pair2(arr, 7);
+console.timeEnd("b")
+const output2 = result.map((r) => `${r[0]}+${r[1]}`).join(", ");
+console.log(output2);
